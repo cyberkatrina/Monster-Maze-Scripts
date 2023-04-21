75fb82518b6c1169f49796c43e32bf358e9e931e
Katsiaryna Aliashkevich <113384816+cyberkatrina@users.noreply.github.com>
2023-04-21 13:06:07 -0500

    Update CatController.cs

--- CatController.cs
+++ CatController.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class CatController : MonoBehaviour
 {
     public bool catTalked = false;
-    // public FloatingTextManager floatingTextManager;
     public GameObject uiObject;
     public Renderer rend;
 
@@ -25,35 +24,22 @@ public class CatController : MonoBehaviour
         }
     }
 
-    // public void ShowText(string msg, int fontSize, Color color, Vector3 position, Vector3 motion, float duration)
-    //     {
-    //         floatingTextManager.Show(msg, fontSize, color, position, motion, duration);
-    //     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        // if (Input.GetKeyDown(KeyCode.Return))
-        // {
             if (collision.tag == "Player" && catTalked == true)
             {
                 uiObject.SetActive(true);
-                //Destroy(gameObject);
                 rend.enabled = false;
                 StartCoroutine("WaitForSec");
-                // ShowText("Congratulations! You found your cat", 36, Color.white, transform.position, Vector3.up * 50, 3.0f);
             }
-        // }
     }
 
     IEnumerator WaitForSec()
     {
-        //gameObject.SetActive(false);
         yield return new WaitForSeconds(5);
         uiObject.SetActive(false);
         Destroy(uiObject);
         Destroy(gameObject);
-    }
-
-
-    
+    }    
 }
